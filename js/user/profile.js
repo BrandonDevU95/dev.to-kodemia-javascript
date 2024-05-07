82de093ec031ac2e0b3126ac5747b3f7b8e694ca
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-05-07 13:52:22 -0600

    Refactor profile page navigation tabs for improved user experience

--- js/user/profile.js
+++ js/user/profile.js
@@ -1,14 +1,63 @@
-import { getUserByUsername, getUserData, logout } from '../api/usersAPI.js';
+import {
+	getToken,
+	getUserByUsername,
+	getUserData,
+	logout,
+} from '../api/usersAPI.js';
 
 const btnLogout = document.getElementById('logout');
 const avatar = document.querySelectorAll('#avatar-image');
+
+const profile = document.getElementById('my-profile');
+const posts = document.getElementById('my-posts');
+const collections = document.getElementById('my-collections');
+
+const profileTab = document.getElementById('profile-tab');
+const postsTab = document.getElementById('posts-tab');
+const collectionsTab = document.getElementById('collections-tab');
+
+if (!getToken()) window.location.href = '../../index.html';
+
+const { user } = getUserData();
 
 btnLogout.addEventListener('click', () => {
 	logout();
 	window.location.href = '../../index.html';
 });
 
-const { user } = getUserData();
+profile.addEventListener('click', () => {
+	profile.querySelector('a').classList.add('active');
+	posts.querySelector('a').classList.remove('active');
+	collections.querySelector('a').classList.remove('active');
+
+	profileTab.classList.contains('d-none') &&
+		profileTab.classList.remove('d-none');
+	postsTab.classList.add('d-none');
+	collectionsTab.classList.add('d-none');
+});
+
+posts.addEventListener('click', () => {
+	posts.querySelector('a').classList.add('active');
+	profile.querySelector('a').classList.remove('active');
+	collections.querySelector('a').classList.remove('active');
+
+	postsTab.classList.contains('d-none') &&
+		postsTab.classList.remove('d-none');
+
+	profileTab.classList.add('d-none');
+	collectionsTab.classList.add('d-none');
+});
+
+collections.addEventListener('click', () => {
+	collections.querySelector('a').classList.add('active');
+	profile.querySelector('a').classList.remove('active');
+	posts.querySelector('a').classList.remove('active');
+
+	collectionsTab.classList.contains('d-none') &&
+		collectionsTab.classList.remove('d-none');
+	profileTab.classList.add('d-none');
+	postsTab.classList.add('d-none');
+});
 
 (async () => {
 	const userObject = await getUserByUsername(user);

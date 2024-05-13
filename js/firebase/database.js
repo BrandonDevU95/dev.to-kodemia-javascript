a0cd23e8b096dddc41c9dedfbeadb6c4973d997d
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-05-12 18:43:16 -0600

    refactor: Update Firebase database initialization and import paths

--- js/firebase/database.js
+++ js/firebase/database.js
@@ -4,12 +4,9 @@ import {
 	ref,
 	set,
 } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-database.js';
+
 import { app } from './config.js';
-
 import { showToast } from '../components/toast.js';
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 const database = getDatabase(app);
 

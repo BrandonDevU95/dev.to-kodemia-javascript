4241653f4b4af619fbc2154c9fdd248af17538ca
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-05-12 18:41:08 -0600

    refactor: Update Firebase configuration and import paths for initialization

--- js/firebase/auth.js
+++ js/firebase/auth.js
@@ -1,5 +1,3 @@
-// Import the functions you need from the SDKs you need
-
 import {
 	createUserWithEmailAndPassword,
 	getAuth,
@@ -9,24 +7,9 @@ import {
 	updateProfile,
 } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js';
 
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js';
+import { app } from './config.js';
 import { showToast } from '../components/toast.js';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-	apiKey: 'AIzaSyCyIfXSzs07eT1Yn5iuTvsyRpk8a93Hi4c',
-	authDomain: 'kodemia-devto.firebaseapp.com',
-	databaseURL: 'https://kodemia-devto-default-rtdb.firebaseio.com',
-	projectId: 'kodemia-devto',
-	storageBucket: 'kodemia-devto.appspot.com',
-	messagingSenderId: '239903884694',
-	appId: '1:239903884694:web:20594aa21b081e7bdb09b7',
-};
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 const signUpFirebase = async (user) => {
--- js/firebase/database.js
+++ js/firebase/database.js
@@ -1,17 +1,16 @@
-import { getDatabase } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-database.js';
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js';
+import {
+	get,
+	getDatabase,
+	ref,
+	set,
+} from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-database.js';
+import { app } from './config.js';
 
-// TODO: Replace the following with your app's Firebase project configuration
-// See: https://firebase.google.com/docs/web/learn-more#config-object
-const firebaseConfig = {
-	// ...
-	// The value of `databaseURL` depends on the location of the database
-
-	databaseURL: 'https://kodemia-devto-default-rtdb.firebaseio.com/',
-};
+import { showToast } from '../components/toast.js';
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Realtime Database and get a reference to the service
-export const database = getDatabase(app);
+const database = getDatabase(app);
+
+export { database };
